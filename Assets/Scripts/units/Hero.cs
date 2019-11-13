8adb4647970eb2bc20013bbb7ecedf25dfc12880
KaleoFeng <kaleofeng@gmail.com>
2019-11-13 20:16:57 +0800

    Prefabs hero

---
+++ Assets/Scripts/units/Hero.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hero : MonoBehaviour {
+
+    public GameObject shield;
+    public GameObject weapon;
+    public Slider healthSlider;
+
+    // Start is called before the first frame update
+    void Start() {
+        shield.GetComponent<Module>().Activate();
+        weapon.GetComponent<Module>().Activate();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
---
+++ Assets/Scripts/units/Module.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Module : MonoBehaviour {
+
+    public bool On { get; private set; } = false;
+    private bool active = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        On = false;
+        active = false;
+        transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Activate() {
+        if (!active) {
+            active = true;
+            transform.DOScale(new Vector3(1, 1, 1), 2f).OnComplete(() => On = true);
+        }
+    }
+
+    public void Deactivate() {
+        if (active) {
+            active = false;
+            transform.DOScale(new Vector3(0, 0, 0), 2f).OnComplete(() => On = false);
+        }
+    }
+}
